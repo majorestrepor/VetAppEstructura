515f3fc8b3fd63928be213d09a433b6b59c15ad7
maicol <165244378+maicols04@users.noreply.github.com>
2024-09-25 11:25:14 -0500

    Primer commit, se ha creado la clase Mascota

---
+++ claseMascota.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+struct mascota {
+	string nombre;
+	string especie;
+	int edad;
+	float peso;
+};
+
+void crearMascota() {
+	
+};
+
+void actualizarMascota() {
+	
+};
+
+void eliminarMascota() {
+	
+};
