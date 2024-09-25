290433f9dd935e53335fd966b00510e1be11008d
Maria Jose <majorr1017@gmail.com>
2024-09-25 11:44:16 -0500

    Se creó la clase cita

---
+++ claseCita.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+struct cita{
+	string fecha;
+	string animal;
+	string tipoCita;
+};
+
+void crearCita(){
+	
+	
+	
+};
+
+void eliminarCita(){
+	
+	
+	
+};
+
+void editarCita(){
+	
+	
+	
+	
+};
