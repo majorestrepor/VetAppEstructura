7665ca876049b89bee2d7b7307c0c7b895fd7275
maicol <165244378+maicols04@users.noreply.github.com>
2024-10-01 15:58:05 -0500

    Se creo la clase VetAppPrincipal y se incluyeron opciones en la clase menu.

---
+++ VetAppPrincipal.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "claseMenu.cpp"
+using namespace std;
+
+int main(int argc, char** argv) {
+	mostrarMenu();
+	
+	return 0;
+}
--- claseMenu.cpp
+++ claseMenu.cpp
@@ -3,11 +3,48 @@
 
 using namespace std;
 
-int main(int argc, char** argv) {
-	
-	Persona persona;
-	leerPersona(persona);
-	imprimirPersona(persona);
-	
-	return 0;
+void mostrarMenu() {
+    int opcion = 0;
+    do {
+        cout << "---------- VETAPP ----------" << endl;
+        cout << "1. Agregar usuario" << endl;
+        cout << "2. Actualizar usuario" << endl;
+        cout << "3. Eliminar usuario" << endl;
+        cout << "4. Buscar usuario" << endl;
+        cout << "5. Agendar cita" << endl;
+        cout << "6. Salir" << endl;
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
+
+        switch (opcion) {
+            case 1: {
+                cout << "Opcion 1: Agregar usuario" << endl;
+            	agregarUsuario();
+                break;
+            }
+            case 2:
+                cout << "Opcion 2: Actualizar usuario" << endl;
+                break;
+            case 3:
+                cout << "Opcion 3: Eliminar usuario" << endl;
+                break;
+            case 4:
+                cout << "Opcion 4: Buscar usuario" << endl;
+                break;
+            case 5:
+                cout << "Opcion 5: Agendar cita" << endl;
+                break;
+            case 6:
+                cout << "Saliendo del programa..." << endl;
+                break;
+            default:
+                cout << "Opcion no valida. Por favor, intente de nuevo." << endl;
+        }
+    } while (opcion != 6);
+}
+
+void agregarUsuario() {
+	Persona persona; 
+    leerPersona(persona);
+    imprimirPersona(persona); 
 }
