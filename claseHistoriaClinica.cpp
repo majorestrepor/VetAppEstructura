1ca19f3865cdf6dd44e5692b67fbae6966078a87
maicol <165244378+maicols04@users.noreply.github.com>
2024-10-02 23:36:07 -0500

    Se realizaron modificaciones a la clase historiClinica

--- claseHistoriaClinica.cpp
+++ claseHistoriaClinica.cpp
@@ -14,12 +14,47 @@ struct HistoriaClinica {
     string descripcionProcedimiento;
 };
 
-void crearHistoriaClinica() {
+void crearHistoriaClinica(HistoriaClinica historia) {
 	
+    cout << "Ingrese el ID de la historia clinica: ";
+    cin >> historia.idHistoria;
+
+    cout << "Ingrese los datos del cliente:" << endl;
+    leerPersona(historia.cliente);
+
+    cout << "Ingrese los datos del veterinario:" << endl;
+    //leerVeterinario(historia.veterinario);
+
+    cout << "Ingrese la fecha del procedimiento:" << endl;
+    crearFecha(historia.fecha);
+
+    cout << "Ingrese el costo del procedimiento: ";
+    cin >> historia.costoProcedimiento;
+
+    cout << "Ingrese la descripcion del procedimiento: ";
+    cin.ignore(); 
+    getline(cin, historia.descripcionProcedimiento);
+
+    cout << "Historia clinica creada con exito." << endl;
 }
 
-void mostrarHistoriaClinica() {
-	
+void mostrarHistoriaClinica(HistoriaClinica historia) {
+    cout << "----- Historia Clinica -----" << endl;
+    cout << "ID de Historia: " << historia.idHistoria << endl;
+
+    cout << "Datos del Cliente:" << endl;
+    imprimirPersona(historia.cliente); 
+
+    cout << "Datos del Veterinario:" << endl;
+   // imprimirVeterinario(historia.veterinario); 
+
+    cout << "Fecha del Procedimiento: " << historia.fecha.dia << "/" 
+         << historia.fecha.mes << "/" << historia.fecha.year << endl;
+
+    cout << "Costo del Procedimiento: " << historia.costoProcedimiento << endl;
+    cout << "Descripcion del Procedimiento: " << historia.descripcionProcedimiento << endl;
+
+    cout << "----------------------------" << endl;
 }
 
 void actualizarHistoriaClinica() {
